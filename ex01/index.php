3c53c78339d3ee4c77aad6e313b82d07b5a64fdd
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2015-04-10 13:58:20 +0200

    ex01

---
+++ ex01/index.php
@@ -0,0 +1,23 @@
+
+<?php
+session_start();
+if ($_GET['submit'] === 'OK')
+{
+	$_SESSION['login'] = $_GET['login'];
+	$_SESSION['passwd'] = $_GET['passwd'];
+}
+$login = $_SESSION['login'];
+$passwd = $_SESSION['passwd'];
+echo <<<EOT
+<html><body>
+<form method="get" action="index.php">
+	 Identifiant: <input type="text" name="login" value="$login"/>
+	 <br />
+	 Mot de passe: <input type="password" name="passwd" value="$passwd"/>
+    <br />
+	<input type="submit" name="submit" value="OK" />
+</form>
+</body></html>
+
+EOT;
+?>
