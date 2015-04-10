0e83a21ff704f59042c63fcafbb79721052e5ed9
Tanguy Gauvrit <tanguygauvrit@gmail.com>
2015-04-10 20:38:17 +0200

    ex03

---
+++ ex03/index.php
@@ -0,0 +1,11 @@
+<html><body>
+<form method="post" action="modif.php">
+	 Identifiant: <input type="text" name="login"/>
+	 <br />
+	 Ancien mot de passe: <input type="password" name="oldpw"/>
+	 <br />
+	 Nouveau mot de passe: <input type="password" name="newpw"/>
+	<br />
+	<input type="submit" name="submit" value="OK" />
+</form>
+</body></html>
---
+++ ex03/modif.php
@@ -0,0 +1,36 @@
+<?php
+if ($_POST['login'] !== NULL &&
+	$_POST['login'] !== ""   &&
+	$_POST['oldpw'] !== NULL &&
+	$_POST['oldpw'] !== ""   &&
+	$_POST['newpw'] !== NULL &&
+	$_POST['newpw'] !== ""   &&
+	$_POST['submit'] === "OK")
+{
+	if (!file_exists("../private/passwd")) {
+		echo "ERROR\n";
+		return ;
+	}
+	$passwd_file = file_get_contents("../private/passwd");
+	$passwd_database = unserialize($passwd_file);
+	foreach ($passwd_database as $key => $data)
+	{
+		if ($data['login'] === $_POST['login']) {
+			$found_key = $key;
+			break ;
+		}
+	}
+	if ($found_key === NULL || $passwd_database[$found_key]['passwd'] !== hash("whirlpool", $_POST['oldpw'])) {
+		echo "ERROR\n";
+		return ;
+	}
+	$passwd_database[$found_key]['passwd'] = hash("whirlpool", $_POST['newpw']);
+	$_POST['oldpw'] = ""; // Because trash data.
+	$_POST['newpw'] = ""; // Because trash data.
+	$passwd_file = serialize($passwd_database);
+	file_put_contents("../private/passwd", $passwd_file);
+	echo "OK\n";
+}
+else
+	echo "ERROR\n";
+?>
